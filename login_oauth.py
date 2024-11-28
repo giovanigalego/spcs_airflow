272819a86eada42dc17ef388323995400559de61
Giovani Galego <giovanigalego96@gmail.com>
2024-11-28 15:11:24 -0300

    Add files via upload

---
+++ login_oauth.py
@@ -0,0 +1,13 @@
+import snowflake.connector
+import os
+
+def get_login_token():
+  with open('/snowflake/session/token', 'r') as f:
+    return f.read()
+
+conn = snowflake.connector.connect(
+  host = os.getenv('SNOWFLAKE_HOST'),
+  account = os.getenv('SNOWFLAKE_ACCOUNT'),
+  token = get_login_token(),
+  authenticator = 'oauth'
+)
