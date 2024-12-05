b54fda3bf5a6e6fef516fe33af9cc3bb763fe0af
Giovani Galego <giovanigalego96@gmail.com>
2024-12-05 18:30:35 -0300

    Add files via upload

---
+++ check_path.py
@@ -0,0 +1,23 @@
+import os
+from datetime import timedelta,datetime
+from airflow import DAG
+#from infra.common import airflow_slack_alerts
+from airflow.operators.bash_operator import BashOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 1)
+}
+
+
+# Create the DAG with the specified schedule interval
+dag = DAG('dbt_dag', default_args=default_args, schedule_interval=timedelta(days=1))
+# Define the dbt run command as a BashOperator
+
+run_script = BashOperator(
+    task_id="check_path",
+    bash_command="ls- l /opt/airflow/dbt/teste_equatorial",
+    dag=dag
+)
