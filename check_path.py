4928f3673b94d78ccdc4a013b00c9313028134a4
Giovani Galego <giovanigalego96@gmail.com>
2024-12-10 15:38:02 -0300

    Update check_path.py

--- check_path.py
+++ check_path.py
@@ -21,5 +21,5 @@ with DAG(
 ) as dag:
     
    run_dbt_model = BashOperator(task_id='list_path', 
-                        bash_command = "ls -l /snowflake/ && ls -l /snowflake/session/ && ls -l /snowflake/session/data/ && printenv"
+                        bash_command = "printenv && ls -l /snowflake/"
 )
